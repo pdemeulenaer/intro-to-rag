53630ebb97a73ab0b7ace8357335defab6905e52
pdemeulenaer <pdemeulenaer@gmail.com>
2025-05-01 15:41:20 +0300

    add comment for clarity on fallback model in get_vectorstore function

--- app-rag-conversation/utils.py
+++ app-rag-conversation/utils.py
@@ -142,7 +142,7 @@ def get_vectorstore(text_chunks):
         "sentence-transformers/clip-ViT-B-32",  # Primary model HF API NOT WORKING AS OF NOW
         "laion/CLIP-ViT-B-32-laion2B-s34B-b79K",  # Fallback model HF API NOT WORKING AS OF NOW
         "openai/clip-vit-base-patch32",  # Another fallback HF API NOT WORKING AS OF NOW
-        "Salesforce/blip-image-captioning-base",
+        "Salesforce/blip-image-captioning-base", # Another fallback HF API NOT WORKING AS OF NOW
         "sentence-transformers/all-MiniLM-L6-v2" # embedding model for TEXT ONLY        
     ]
     
